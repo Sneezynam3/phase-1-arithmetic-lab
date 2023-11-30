c69758cda696dd8c69adb90e8d0dc341f6d5aed3
James Snell <vulpes3.1415@yahoo.com>
2023-11-29 19:22:53 -0700

    Arithmetic Lab Complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 12;
+let num4 = 3;
+//variable declarations
+
+let multiply = num1 * num2;
+
+let random = parseInt(Math.random() * 5 + 1);
+//parseInt=typeCasting (changing typeOf)
+let mod = num3 / num4;
+
+let max = Math.max((21, 10, 15, 20));
+// finding highest number in a set 
+
+
+
+
